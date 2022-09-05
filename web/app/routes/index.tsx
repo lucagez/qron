128dea05a9a79d46f7c0aeb3a972a2f81e3f59ab
Luca Gesmundo <lucagesmundo@yahoo.it>
2022-09-05 10:06:56 +0200

    init web

---
+++ web/app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Cloudflare pages setup 🦄</h1>
+    </div>
+  );
+}
