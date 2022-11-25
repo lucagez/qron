44c1fc6dd1e153efe24ddb64594ea04a5d1f905a
lucagez <lucagesmundo@yahoo.it>
2022-11-25 12:38:59 +0100

    delete web

--- web/app/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Cloudflare pages setup 🦄</h1>
-    </div>
-  );
-}
